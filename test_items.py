024f2e94074f8825aacf77f99cd280006b3d2d74
dmitriy.khvorostyano <dmitriy.khvorostyanoy@p2h.com>
2021-11-25 17:57:37 +0200

    code_refactoring

--- test_items.py
+++ test_items.py
@@ -1,9 +1,7 @@
-import time
-
 link = ' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_presense_of_button_for_es_lang(browser):
+def test_presence_of_button_for_es_lang(browser):
     browser.get(link)
     a = browser.find_elements_by_css_selector("#add_to_basket_form [type='submit']")
     assert 1 == len(a), 'button is not exist'
